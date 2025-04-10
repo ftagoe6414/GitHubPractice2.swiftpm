fdb2d0ee939a5f8b162817491e146fb17a3fe07a
Luca Fabian <lfabian8121@stu.d214.org>
2025-04-10 11:10:56 -0500

    added button changed some code

--- ContentView.swift
+++ ContentView.swift
@@ -3,6 +3,7 @@ import SwiftUI
 struct ContentView: View {
     @State var number1 = 0
     @State var number2 = 0
+    @State var answer = 0
     var body: some View {
         VStack {
             
@@ -10,7 +11,13 @@ struct ContentView: View {
             TextField("Enter a Number", value: $number2, format: .number)
             
             TextField("Enter a Number", value: $number1, format: .number)
-            @State var answer = number1 + number2
+            
+            Button("Add"){
+                
+                answer = number1 + number2
+                
+                }
+          
             Text("\(answer)")
         }
     }
